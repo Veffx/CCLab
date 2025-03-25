4e0f2fe41e8e75bea903db70c0fc4c5c4533ad50
Veffx <1192849033@qq.com>
2025-03-25 17:02:28 +0800

    y

--- array-1/sketch.js
+++ array-1/sketch.js
@@ -1,4 +1,4 @@
-let greetings =["Hallo","哈罗,你吃了吗","你好",'1']
+let greetings =["Hallo","哈罗,你吃了吗","你好"]
 
 function setup() {
   let canvas = createCanvas(800, 500);
@@ -7,7 +7,8 @@ function setup() {
 
 function draw() {
   background(10,210,230);
-  for(let i = 0;i<3;i++){
-    text(greetings[i],width/2,heigth/2+i*12)
+  // text(greetings[1],width/2,height/2);
+  for(let i = 0;i<greetings.length;i++){
+    text(greetings[i],width/2,height/2+i*12)
   }
 }
