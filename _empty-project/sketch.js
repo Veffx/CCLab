ae081d0c70c8f98d3eddd5b8769ff2e9629070a1
Veffx <1192849033@qq.com>
2025-04-08 11:58:18 +0800

    yep

--- _empty-project/sketch.js
+++ _empty-project/sketch.js
@@ -1,3 +1,4 @@
+let egg1;
 function setup() {
   let canvas = createCanvas(800, 500);
   canvas.parent("p5-canvas-container");
@@ -5,4 +6,32 @@ function setup() {
 
 function draw() {
   background(220);
+  egg1.update()
+  egg1.dispaly()
+}
+class Egg{
+  constructor(){
+    this.x=200;
+    this.y=200;
+    this.radius =50;
+    this.speedx = 1;
+    this.speedy = 1;
+    }
+    update(){
+      this.x+=this.speedx;
+      this.y+=this.speedy;
+      if(this.x<0||this.x>width){
+        this.speedx= -this.speedx;
+      }
+      if(this.y<0||this.y>height){
+        this.speedy= -this.speedy;
+      }
+    }
+  dispaly(){
+    push();
+    translate(this.x,this.y);
+    circle(0,0,this.radius);
+    pop();
+  }
+  
 }
--- array-1/sketch.js
+++ array-1/sketch.js
@@ -7,32 +7,5 @@ function setup() {
 
 function draw() {
   background(10,210,230);
-  egg1.update()
-  egg1.dispaly()
-}
-class Egg{
-  constructor(){
-    this.x=200;
-    this.y=200;
-    this.radius =50;
-    this.speedx = 1;
-    this.speedy = 1;
-    }
-    update(){
-      this.x+=this.speedx;
-      this.y+=this.speedy;
-      if(this.x<0||this.x>width){
-        this.speedx= -this.speedx;
-      }
-      if(this.y<0||this.y>height){
-        this.speedy= -this.speedy;
-      }
-    }
-  dispaly(){
-    push();
-    translate(this.x,this.y);
-    circle(0,0,this.radius);
-    pop();
-  }
   
 }
