f37e68707e83075ce08dbeb7907c0599300757cb
Veffx <1192849033@qq.com>
2025-04-08 11:55:30 +0800

    1

--- array-1/sketch.js
+++ array-1/sketch.js
@@ -1,4 +1,4 @@
-let greetings =["Hallo","哈罗,你吃了吗","你好"]
+let egg1;
 
 function setup() {
   let canvas = createCanvas(800, 500);
@@ -7,8 +7,32 @@ function setup() {
 
 function draw() {
   background(10,210,230);
-  // text(greetings[1],width/2,height/2);
-  for(let i = 0;i<greetings.length;i++){
-    text(greetings[i],width/2,height/2+i*12)
+  egg1.update()
+  egg1.dispaly()
+}
+class Egg{
+  constructor(){
+    this.x=200;
+    this.y=200;
+    this.radius =50;
+    this.speedx = 1;
+    this.speedy = 1;
+    }
+    update(){
+      this.x+=this.speedx;
+      this.y+=this.speedy;
+      if(this.x<0||this.x>width){
+        this.speedx= -this.speedx;
+      }
+      if(this.y<0||this.y>height){
+        this.speedy= -this.speedy;
+      }
+    }
+  dispaly(){
+    push();
+    translate(this.x,this.y);
+    circle(0,0,this.radius);
+    pop();
   }
+  
 }
